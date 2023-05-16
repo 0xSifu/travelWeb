ccd06db6cce658aefee13f31e49a5e968e24c8f0
Ferryal Fajar <ferryal@dagangan.com>
2023-05-16 16:36:08 +0700

    fixing error

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return <h1 style={{
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '3rem'
+    }}>Login Component</h1>
+}
+
+export default Login;
